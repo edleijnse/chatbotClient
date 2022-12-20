8684b4f1cd98dbb9f59f70f0ae9a0fc398ff0d3f
edleijnse <ed@leijnse.info>
2022-12-20 14:57:01 +0100

    GUI sample added without functionality

--- main.py
+++ main.py
@@ -2,8 +2,11 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
+import tkinter as tk
+def callChatWithOpenAI():
+    # chatwithOpenAI(question)
+    print("question: " + question)
+    print("answer: " + chatwithOpenAI(question))
 def chatwithOpenAI(question):
     # Use a breakpoint in the code line below to debug your script.
     import os
@@ -38,11 +41,49 @@ def chatwithOpenAI(question):
     # Print the text
     #print(text)
     return text
+    # Place the label, text field, and button in the window
+
+
+
+
+# Example usage
+def copy_text():
+  input_text = input_text_field.get()
+  output_text_field.delete(0, tk.END)
+  output_text_field.insert(0, input_text)
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     question = "Bitte schreibe einen Email auf Deutsch an Frau Koepli. Ich möchte mitteilen, dass die Heizung zu hoch abgestellt is: der Zimmertemperatur ist 24.5 Grad. Gebäude:Büttenenhalde 37, Apartment 38. Bei dieser Gelegenheit möchte ich Frau Koepli auch eine schöne Weihnachtszeit zuwünschen und ein frohes Neues Jahr."
 
-    print(chatwithOpenAI(question))
+import tkinter as tk
+
+# Create the main window
+window = tk.Tk()
+window.title("Text Field Example")
+
+# Create a label and an input text field
+input_label = tk.Label(window, text="Enter some text:")
+input_text_field = tk.Entry(window)
+
+# Create an output label and an output text field
+output_label = tk.Label(window, text="Output:")
+output_text_field = tk.Entry(window)
+
+# Create a button
+button = tk.Button(window, text="Copy", command=copy_text)
+
+# Place the labels and text fields in the window
+input_label.pack()
+input_text_field.pack()
+output_label.pack()
+output_text_field.pack()
+button.pack()
+
+# Run the main loop
+window.mainloop()
+
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
